e6ced24df7bf7eb8b06b899e56f046338ebe00dc
dinarazafy <dinasly01@gmail.com>
2022-12-22 17:47:56 +0300

    Initial commit

---
+++ src/main/java/com/gitdec/gitdecemberv2/GitDecemberv2Application.java
@@ -0,0 +1,13 @@
+package com.gitdec.gitdecemberv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDecemberv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDecemberv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gitdec/gitdecemberv2/GitDecemberv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitdec.gitdecemberv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDecemberv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
